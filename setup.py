b522de750a8e7b4a1071765e79c62e534c05beed
Jordan Pownall <jord_pownall@hotmail.co.uk>
2022-06-25 12:07:38 +0100

    First commitgit add .git add .!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A quantitative analysis and trading of crypto currencies.',
+    author='Pownall Boyz',
+    license='MIT',
+)
